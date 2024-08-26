f8cbc5bd02b73f839ce1568bb1f50aa026652802
Muhammed Ashfaaq <bma94.7@gmail.com>
2024-08-26 11:26:42 +0100

    fix: correct typo

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
-        extra_kwargs = {"password": {"write_onlys": True}}
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_frmework',
+    'rest_framework',
     'corsheaders',
 ]
 
